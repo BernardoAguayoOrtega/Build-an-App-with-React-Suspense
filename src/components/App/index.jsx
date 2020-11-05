6fc2570c36dc780fd829917c39c2eb6a3add1f1e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-05 11:01:13 -0600

    Use my first react suspense component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,18 @@
 // import react
 import React from "react";
 // import pokemon detail
-import PokemonDetail from "../PokemonDetail/index.jsx";
+// import PokemonDetail from "../PokemonDetail/index.jsx";
+
+// import dynamic of pokemon detail
+const PokemonDetail = React.lazy(() => import("../PokemonDetail/index.jsx"))
 
 export default function App() {
   return (
-    <div>
-      <PokemonDetail />
-    </div>
+    <>
+      <h1>Pokedex</h1>
+      <React.Suspense fallback='Loading pokemon'>
+        <PokemonDetail />
+      </React.Suspense>
+    </>
   );
 }
