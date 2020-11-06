c47776bfc80365e41f451c89ef41cba888d0467e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-06 12:02:04 -0600

    Add suspnedly high order function to our fetch

--- src/components/PokemonDetail/index.jsx
+++ src/components/PokemonDetail/index.jsx
@@ -1,6 +1,41 @@
-// import react
-import React from 'react';
+
+import React from "react";
+
+// Wrap Fetch Requests to Communicate Pending, Error and Success Status to React Suspense
+
+function suspensify(promise) {
+  let status = "pending";
+  let result;
+  let suspender = promise.then(
+    response => {
+      status = "success";
+      result = response;
+    },
+    error => {
+      status = "error";
+      result = error;
+    }
+  );
+
+  return {
+    read() {
+      if (status === "pending") {
+        throw suspender;
+      }
+      if (status === "error") {
+        throw result;
+      }
+      if (status === "success") {
+        return result;
+      }
+    }
+  };
+}
+
+let pokemon = suspensify(
+  fetch(`https://pokeapi.co/api/v2/pokemon/1`).then(res => res.json())
+);
 
 export default function PokemonDetail() {
-	return <div>Static Pokemon</div>;
+  return <div>{pokemon.read().name}</div>;
 }
