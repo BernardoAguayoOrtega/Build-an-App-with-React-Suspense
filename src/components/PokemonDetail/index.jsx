1b0a3b5c5e122e4b107fb4bee24c09c0c6d5abd1
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-09 12:24:32 -0600

    Change function to find only pokemon

--- src/components/PokemonDetail/index.jsx
+++ src/components/PokemonDetail/index.jsx
@@ -1,40 +1,40 @@
-
-import React from "react";
+import React from 'react';
 
 // Wrap Fetch Requests to Communicate Pending, Error and Success Status to React Suspense
 
 function suspensify(promise) {
-  let status = "pending";
+  let status = 'pending';
   let result;
   let suspender = promise.then(
-    response => {
-      status = "success";
+    (response) => {
+      status = 'success';
       result = response;
     },
-    error => {
-      status = "error";
+    (error) => {
+      status = 'error';
       result = error;
     }
   );
 
   return {
     read() {
-      if (status === "pending") {
+      if (status === 'pending') {
         throw suspender;
       }
-      if (status === "error") {
+      if (status === 'error') {
         throw result;
       }
-      if (status === "success") {
+      if (status === 'success') {
         return result;
       }
-    }
+    },
   };
 }
 
-let pokemon = suspensify(
-  fetch(`https://pokeapi.co/api/v2/pokemon/1`).then(res => res.json())
-);
+const fetchPokemon = (id) =>
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => res.json());
+
+let pokemon = suspensify(fetchPokemon(50));
 
 export default function PokemonDetail() {
   return <div>{pokemon.read().name}</div>;
